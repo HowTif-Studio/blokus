1ea348783ee507cc1a5547aaf6ceac5161cf066d
Howard 張浩銘 <howard.zhang@foxconn.com>
2024-01-01 22:32:28 +0800

    [delete] .settings .classpath .project [modified] 讓遊戲一開始不會跳兩次選擇人數的視窗

--- src/View/Choose_Dialog.java
+++ src/View/Choose_Dialog.java
@@ -9,50 +9,54 @@ import java.awt.event.*;
 public class Choose_Dialog {
 	
 	//JFrame frame = new JFrame();
+	Home_Panel home_panel = new Home_Panel();
 	
 	public JDialog Choose_Dialog(Frame owner, String title, boolean modal){
-		
+
 		JDialog dialog = new JDialog();
-		
-		String players[] = { "1" , "2" , "3" , "4" };  
-		JComboBox cb = new JComboBox(players);  
-		
-		dialog.setLayout( new FlowLayout() );  
-		dialog.add(new JLabel ("請選擇玩家人數"));
-		
-		cb.setBounds(50,50,50,20);
-		dialog.add(cb);
-		
-		JButton b1 = new JButton ("開始遊戲");  
-		JButton b2 = new JButton ("離開");
-		
-		
-		b1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // 在此處放置按鈕被點擊時的行為
-            	//frame.setVisible(true);
-            	
-//            	Game_Frame game_frame = new Game_Frame(); // 創建 Game_Frame 的實例
-//            	game_frame.createAndShowGUI(); // 顯示 Game_Frame 中的 frame
-//                dialog.dispose(); // 關閉目前的對話框
-            	
-            }
-		});
-		
-		b2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // 在此處放置按鈕被點擊時的行為
-            	dialog.dispose();
-            }
-		});
-		
-		        
-		dialog.add(b1);
-		dialog.add(b2);
-		dialog.setSize(200, 200);
-		dialog.setVisible(true);
-		
-		return dialog;
+
+		if(home_panel.Home() != 1){
+
+			String players[] = { "1" , "2" , "3" , "4" };
+			JComboBox cb = new JComboBox(players);
+
+			dialog.setLayout( new FlowLayout() );
+			dialog.add(new JLabel ("請選擇玩家人數"));
+
+			cb.setBounds(50,50,50,20);
+			dialog.add(cb);
+
+			JButton b1 = new JButton ("開始遊戲");
+			JButton b2 = new JButton ("離開");
+
+
+			b1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					// 在此處放置按鈕被點擊時的行為
+					//frame.setVisible(true);
+
+					Game_Frame game_frame = new Game_Frame(false); // 創建 Game_Frame 的實例
+	//            	game_frame.createAndShowGUI(); // 顯示 Game_Frame 中的 frame
+	//                dialog.dispose(); // 關閉目前的對話框
+
+				}
+			});
+
+			b2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					// 在此處放置按鈕被點擊時的行為
+					dialog.dispose();
+				}
+			});
+			dialog.add(b1);
+			dialog.add(b2);
+			dialog.setSize(200, 200);
+			dialog.setVisible(true);
+		} else {
+			dialog.setVisible(false);
 		}
+
+		return dialog;
+	}
 	
 }
--- src/View/Game_Frame.java
+++ src/View/Game_Frame.java
@@ -18,30 +18,28 @@ public class Game_Frame {
 	Choose_Dialog choose_dialog = new Choose_Dialog();
 	
 //	JFrame Game_Frame(){
-	Game_Frame(){
+	Game_Frame(boolean needChoose){
 		
-		if ( home_panel.Home() != 1 ) {
-			
-			frame.add(choose_dialog.Choose_Dialog(frame , "Dialog Example", true));
-			
-				
-			frame.add(players_panel.Panel1());
-            frame.add(players_panel.Panel2());
-            frame.add(players_panel.Panel3());
-            frame.add(players_panel.Panel4());
-            frame.add(chess_panel.Panel5());
-
-            frame.setLayout(null);
-            frame.setSize(1200, 800);
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
-        	};
-			
-//        	return frame;
+        if(needChoose){
+            frame.add(choose_dialog.Choose_Dialog(frame , "Dialog Example", true));
         }
 
+        frame.add(players_panel.Panel1());
+        frame.add(players_panel.Panel2());
+        frame.add(players_panel.Panel3());
+        frame.add(players_panel.Panel4());
+        frame.add(chess_panel.Panel5());
+
+        frame.setLayout(null);
+        frame.setSize(1200, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        };
+
+//        	return frame;
+
     public static void main(String[] args) {
-        new Game_Frame();
+        new Game_Frame(true);
     }
 }
